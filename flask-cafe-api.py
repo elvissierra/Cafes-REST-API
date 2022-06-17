05d7278f1c6e20b01a73b950dfb540483864e6b2
elvis <elvissierra3@gmail.com>
2022-06-17 07:54:01 -0500

    added functions

--- flask-cafe-api.py
+++ flask-cafe-api.py
@@ -7,88 +7,154 @@ import ast
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
-        data = pd.read_csv('user.csv')
+        data = pd.read_csv("user.csv")
         data = data.to_dict()
-        return {'data': data}, 200
+        return {"data": data}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('userId', required = True)
-        parser.add_argument('name', required = True)
-        parser.add_argument('city', required = True)
+        parser.add_argument("userId", required=True)
+        parser.add_argument("name", required=True)
+        parser.add_argument("city", required=True)
         args = parser.parse_args()
 
-        data = pd.read_csv('user.csv')
+        data = pd.read_csv("user.csv")
 
-        if args['userId'] in list(data['userId']):
-            return {
-                'message': f"'{args['userId']}' already exists."
-            }, 409
+        if args["userId"] in list(data["userId"]):
+            return {"message": f"'{args['userId']}' already exists."}, 409
 
         else:
-            new_data = pd.DataFrame({
-                'userId':[args['userId']],
-                'name':[args['name']],
-                'city':[args['city']],
-                'locations':[[]],
-            })
+            new_data = pd.DataFrame(
+                {
+                    "userId": [args["userId"]],
+                    "name": [args["name"]],
+                    "city": [args["city"]],
+                    "locations": [[]],
+                }
+            )
 
-            data = data.append(new_data, ignore_index = True)
-            data.to_csv('user.csv', index= False)
-            return {'data' : data.to_dict()}, 200
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv("user.csv", index=False)
+            return {"data": data.to_dict()}, 200
 
     def put(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('userId', required = True)
-        parser.add_argument('location', required = True)
+        parser.add_argument("userId", required=True)
+        parser.add_argument("location", required=True)
         args = parser.parse_args()
 
         data = pd.read_csv
 
-        if args['userId'] in list(data['userId']):
-            data['locations'] = data['locations'].apply(
-                lambda x: ast.literal_eval(x)
+        if args["userId"] in list(data["userId"]):
+            data["locations"] = data["locations"].apply(lambda x: ast.literal_eval(x))
+
+            user_data = data[data["userId"] == args["userId"]]
+
+            user_data["locations"] = (
+                user_data["locations"].values[0].append(args["location"])
             )
 
-            user_data = data[data['userId'] == args['userId']]
+            data.to_csv("user.csv", index=False)
 
-            user_data['locations'] = user_data['locations'].values[0]\
-                .append(args['location'])
-
-            data.to_csv('user.csv', index=False)
-
-            return {'data': data.to_dict()}, 200
+            return {"data": data.to_dict()}, 200
 
         else:
-            return {
-                'message': f"'{args['userId']}' user not found."
-            }, 404
+            return {"message": f"'{args['userId']}' user not found."}, 404
 
     def delete(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('userId', required = True)
+        parser.add_argument("userId", required=True)
         args = parser.parse_args()
 
-        data = pd.read_csv('users.csv')
+        data = pd.read_csv("users.csv")
 
-        if args['userId'] in list(data['userId']):
-            data = data[data['userId'] != args['userId']]
+        if args["userId"] in list(data["userId"]):
+            data = data[data["userId"] != args["userId"]]
 
-            data.to_csv('user.csv', index= False)
+            data.to_csv("user.csv", index=False)
 
-            return {'data': data.to_dict()}, 200
+            return {"data": data.to_dict()}, 200
         else:
-            return{
-                'message': f"'{args['userId']}'user not found"
-            }, 404
+            return {"message": f"'{args['userId']}'user not found"}, 404
 
 
 class Locations(Resource):
     def get(self):
-        data = pd.read_csv('locations.csv')
-        return {'data': data.to_dict()}, 200
+        data = pd.read_csv("locations.csv")
+        return {"data": data.to_dict()}, 200
 
     def post(self):
-        
+        parser = reqparse.RequestParser()
+        parser.add_argument("locationId", required=True, type=int)
+        parser.add_argument("name", required=True)
+        parser.add_argument("rating", required=True)
+        args = parser.parse_args()
+
+        data = pd.read_csv("locations.csv")
+
+        if args["location"] in list(data["locationId"]):
+
+            return {"message": f"'{args['locationId']}' already exists."}, 409
+
+        else:
+
+            new_data = pd.DataFrame(
+                {
+                    "locationId": [args["locationId"]],
+                    "name": [args["name"]],
+                    "rating": [args["rating"]],
+                }
+            )
+
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv("locations.csv", index=False)
+            return {"data": data.to_dict()}, 200
+
+    def patch(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("locationId", required=True, type=int)
+        parser.add_argument("name", store_missing=False)
+        parser.add_argument("rating", store_missing=False)
+        args = parser.parse_args()
+
+        data = pd.read_csv("locations.csv")
+
+        if args["location"] in list(data["locationId"]):
+            user_data = data[data["locationId"] == args["locationId"]]
+
+            if "name" in args:
+                user_data["name"] = args["name"]
+            if "rating" in args:
+                user_data["rating"] = args["rating"]
+
+            data[data["locationId"] == args["locationId"]] = user_data
+            data.to_csv("location.csv", index=False)
+            return {"data": data.to_dict()}, 200
+
+        else:
+            return {"message": f"'{args['locationId']}' location does not exist"}, 404
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("locationId", required=True, type=int)
+        args = parser.parser.args()
+
+        data = pd.read_csv("locations.csv")
+
+        if args["locationId"] in list(data["locationId"]):
+            data = data[data["locationId"] != args["locationId"]]
+            data.to_csv("locations.csv", index=False)
+            return {"data": data.to_dict()}, 200
+
+        else:
+            return {"message": f"'{args['locationId']}'location does not exist"}
+
+
+api.add_resource(Users, "/users")
+api.add_resource(Locations, "/locations")
+
+if __name__ == "__main__":
+    app.run()
