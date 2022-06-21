e8f139a4081d0770ae0f0f49abb08046a028eb33
elvis <elvissierra3@gmail.com>
2022-06-20 19:26:23 -0500

    .

--- flask-cafe-api.py
+++ flask-cafe-api.py
@@ -20,48 +20,23 @@ class Users(Resource):
         parser.add_argument('city', required=True)
         args = parser.parse_args()
 
-        
-
+        data = pd.read_csv('users.csv')
+    
         if args['userId'] in list(data['userId']):
             return {
                 'message': f"'{args['userId']}' already exists."
             }, 409
         else:
             new_data = pd.DataFrame({
-                'userId': [args['userId']],
-                'name': [args['name']],
-                'city': [args['city']],
-                'locations': [[]]
+                'userId': args['userId'],
+                'name': args['name'],
+                'city': args['city'],
+                'locations': []
             })
-            data = pd.read_csv('users.csv')
-            data = data.append(new_data, ignore_index=True)
+            data = data.append(new_data,ignore_index=True)
             data.to_csv('users.csv', index=False)
             return {'data': data.to_dict()}, 200
 
-    def put(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('userId', required=True)
-        parser.add_argument('location', required=True)
-        args = parser.parse_args()
-
-        data = pd.read_csv('users.csv')
-        
-        if args['userId'] in list(data['userId']):
-            data['locations'] = data['locations'].apply(
-                lambda x: ast.literal_eval(x)
-            )
-            user_data = data[data['userId'] == args['userId']]
-
-            user_data['locations'] = user_data['locations'].values[0] \
-                .append(args['location'])
-            
-            data.to_csv('users.csv', index=False)
-            return {'data': data.to_dict()}, 200
-
-        else:
-            return {
-                'message': f"'{args['userId']}' user not found."
-            }, 404
 
     def delete(self):
         parser = reqparse.RequestParser()
@@ -108,10 +83,35 @@ class Locations(Resource):
             data = data.append(new_data, ignore_index=True)
             data.to_csv('locations.csv', index=False)
             return {'data': data.to_dict()}, 200
-    
+
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('userId', required=True)
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()
+
+        data = pd.read_csv('locations.csv')
+        
+        if args['userId'] in list(data['userId']):
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            user_data = data[data['userId'] == args['userId']]
+
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+            
+            data.to_csv('locations.csv', index=False)
+            return {'data': data.to_dict()}, 200
+
+        else:
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
+
     def patch(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('locationId', required=True, type=int)
+        parser.add_argument('locationId', required=True)
         parser.add_argument('name', store_missing=False)
         parser.add_argument('rating', store_missing=False)
         args = parser.parse_args()
